edc941dc2f9733a99d409b5485c86c91635e1c2c
Andrew Steel <stahlhutandreas@gmail.com>
2025-04-29 16:18:08 +0200

    typo

--- compose/whisper/run.sh
+++ compose/whisper/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-exec /opt/venv/bin/wyoming_faster_whisper\
+exec /opt/venv/bin/wyoming-faster-whisper\
     --uri 'tcp://0.0.0.0:10300' \
     --data-dir /data \
     --download-dir /data "$@"
